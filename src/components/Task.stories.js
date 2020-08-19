af6c27dbfe93ffd2d6a3b18daf6ac214242cf699
= <=>
2020-08-19 18:53:17 +0200

    Added knobs + long title task story

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
+import { withKnobs, object } from '@storybook/addon-knobs';
 
 import Task from './Task';
 
 export default {
   component: Task,
   title: 'Task',
+  decorators: [withKnobs],
   excludeStories: /.*Data$/,
 };
 
@@ -21,7 +23,9 @@ export const actionsData = {
   onArchiveTask: action('onArchiveTask'),
 };
 
-export const Default = () => <Task task={{...taskData}} {...actionsData} />;
+export const Default = () => (
+  <Task task={object('task', { ...taskData })} {...actionsData} />
+);
 
 export const Pinned = () => (
   <Task task={{...taskData, state: 'TASK_PINNED'}} {...actionsData} />
@@ -29,4 +33,9 @@ export const Pinned = () => (
 
 export const Archived = () => (
   <Task task={{...taskData, state: 'TASK_ARCHIVED'}} {...actionsData} />
+);
+
+const longTitle = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam varius tellus elit, id vulputate leo semper vel. Nunc hendrerit velit eget malesuada bibendum. Morbi sed ipsum in lacus pulvinar dapibus. Vestibulum scelerisque arcu id augue lacinia placerat. Integer vitae pharetra ipsum. Suspendisse at malesuada magna. Suspendisse vitae laoreet est. Sed vitae sollicitudin velit, ac rhoncus ipsum. Vestibulum in viverra quam. Nullam sed mi varius, posuere nisi non, finibus lectus. Suspendisse finibus commodo leo, ac posuere nisi elementum a. Duis laoreet orci elementum, maximus felis quis, sollicitudin est. Sed metus dui, varius at tortor quis, sollicitudin lobortis ligula. In hac habitasse platea dictumst. ";
+export const LongTitle = () => (
+  <Task task={object('task', { ...taskData, title: longTitle })} {...actionsData} />
 );
